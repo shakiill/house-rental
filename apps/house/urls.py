eb39b806875c16eb3f9a2766bbe8854485e18abc
Saleh Ahmad Shakil <shakil493888@gmail.com>
2023-12-17 01:36:17 +0600

    sdgf

--- apps/house/urls.py
+++ apps/house/urls.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.decorators import login_required
-from django.urls import path, re_path, include
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('dashboard/', login_required(views.DashboardView.as_view()), name='dashboard'),
     path('', views.HomeView.as_view(), name='home'),
+
+    path('apartment/', login_required(views.ApartmentListView.as_view()), name='apartments'),
+    path('apartment/add/', login_required(views.ApartmentCreateView.as_view()), name='apartment_add'),
+    path('apartment/<int:pk>/update/', login_required(views.ApartmentUpdateView.as_view()),
+         name='apartment_update'),
+    path('apartment/<int:pk>/delete/', login_required(views.ApartmentDeleteView.as_view()),
+         name='apartment_delete'),
 ]
