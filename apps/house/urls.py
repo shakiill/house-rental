3bd25f0dd061d80257a464648c9d8a1e463bd831
Saleh Ahmad Shakil <shakil493888@gmail.com>
2023-12-17 01:39:12 +0600

    sdgf

--- apps/house/urls.py
+++ apps/house/urls.py
@@ -5,12 +5,15 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', login_required(views.DashboardView.as_view()), name='dashboard'),
-    path('', views.HomeView.as_view(), name='home'),
 
-    path('apartment/', login_required(views.ApartmentListView.as_view()), name='apartments'),
-    path('apartment/add/', login_required(views.ApartmentCreateView.as_view()), name='apartment_add'),
-    path('apartment/<int:pk>/update/', login_required(views.ApartmentUpdateView.as_view()),
+    path('dashboard/apartment/', login_required(views.ApartmentListView.as_view()), name='apartments'),
+    path('dashboard/apartment/add/', login_required(views.ApartmentCreateView.as_view()), name='apartment_add'),
+    path('dashboard/apartment/<int:pk>/update/', login_required(views.ApartmentUpdateView.as_view()),
          name='apartment_update'),
-    path('apartment/<int:pk>/delete/', login_required(views.ApartmentDeleteView.as_view()),
+    path('dashboard/apartment/<int:pk>/delete/', login_required(views.ApartmentDeleteView.as_view()),
          name='apartment_delete'),
+    # public
+    path('', views.HomeView.as_view(), name='home'),
+    path('apartment/', views.PublicApartmentListView.as_view(), name='apartments'),
+
 ]
--- apps/house/views.py
+++ apps/house/views.py
@@ -10,22 +10,7 @@ from apps.house.models import Apartment
 
 # Create your views here.
 class DashboardView(TemplateView):
-    template_name = 'home.html'  # Default template
-
-    # def get_template_names(self):
-    #     user = self.request.user
-    #     if user.is_authenticated:
-    #         if Customer.objects.filter(pk=user.pk).exists():
-    #             return ['customer/home.html']
-    #         elif Salesman.objects.filter(pk=user.pk).exists():
-    #             return ['salesman/home.html']
-    #         elif Staff.objects.filter(pk=user.pk).exists() or user.is_superuser:
-    #             return ['home.html']
-    #     return [self.template_name]
-
-
-class HomeView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'home.html'
 
 
 class ApartmentListView(FilterView):
@@ -59,3 +44,17 @@ class ApartmentDeleteView(PageHeaderMixin, DeleteView):
     template_name = 'delete.html'
     page_title = 'apartments'
     success_url = reverse_lazy('apartments')
+
+
+# public
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+
+class PublicApartmentListView(FilterView):
+    model = Apartment
+    filterset_class = ApartmentFilters
+    paginate_by = 20
+    ordering = '-created_at'
+    template_name = 'all_apartments.html'
